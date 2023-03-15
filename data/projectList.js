f60c885d969f39bde2aed1a0e17d088529477ee1
builtbyjosh <joshuacollins912@gmail.com>
2023-03-14 23:52:00 -0500

    add project list sample data for ProjectCards

---
+++ data/projectList.js
@@ -0,0 +1,37 @@
+export const projectsList = [
+    {
+      id: 1,
+      title: 'Project1',
+      logo: 'https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&auto=format&fit=crop&w=334&q=80',
+      link: 'https://www.test.com',
+      desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      technologies: ['rails', 'react']
+    },
+    {
+      id: 2,
+      title: 'Project2',
+      link: 'https://www.test.com',
+      logo: 'https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&auto=format&fit=crop&w=334&q=80',
+      desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.`,
+      technologies: ['ruby', 'javascript']
+    },
+    {
+      id: 3,
+      title: 'Project3',
+      link: 'https://www.test.com',
+      logo: 'https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&auto=format&fit=crop&w=334&q=80',
+      desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.`,
+      technologies: ['ruby', 'javascript']
+    },
+    {
+      id: 4,
+      title: 'Project4',
+      link: 'https://www.test.com',
+      logo: 'https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&auto=format&fit=crop&w=334&q=80',
+      desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.`,
+      technologies: ['ruby', 'javascript']
+    }
+  ];
