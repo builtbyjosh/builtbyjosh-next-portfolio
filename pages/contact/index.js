1717c5cc41c2867e2f04dac44856773bbc38e443
builtbyjosh <joshuacollins912@gmail.com>
2023-03-16 13:23:28 -0500

    add react-hook-form logic and error handling

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,106 +1,125 @@
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 import {
   Box,
   Button,
   Flex,
-  FormControl,
+  Text,
   FormLabel,
   Heading,
-  IconButton,
   Input,
   InputGroup,
   InputLeftElement,
-  Link,
   Stack,
   Textarea,
-  Tooltip,
-  useClipboard,
   VStack,
-} from '@chakra-ui/react';
-import React from 'react';
-import { BsGithub, BsLinkedin, BsPerson, BsTwitter } from 'react-icons/bs';
-import { MdEmail, MdOutlineEmail } from 'react-icons/md';
-import SocialIcons from '../../components/SocialIcons';
+} from "@chakra-ui/react";
+import { BsPerson } from "react-icons/bs";
+import { MdOutlineEmail } from "react-icons/md";
+import SocialIcons from "../../components/SocialIcons";
+import FormErrorMsg from "../../components/FormErrorMsg";
 
 const ContactMe = () => {
-  const { hasCopied, onCopy } = useClipboard('joshuacollins912@gmail.com');
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitting, errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    // e.preventDefault();
+    console.log("formData: ", data);
+  };
 
   return (
-    <Flex
-      align="center"
-      justify="center"      
-      id="contact">
-      <Box        
-        m={{ base: 5, md: 12, lg: 6 }}
-        p={{ base: 5, lg: 12 }}>
+    <Flex align="center" justify="center" id="contact">
+      <Box m={{ base: 5, md: 12, lg: 6 }} p={{ base: 5, lg: 12 }}>
         <Box>
           <VStack spacing={{ base: 4, md: 8, lg: 10 }}>
             <Heading
               fontSize={{
-                base: '4xl',
-                md: '5xl',
-              }}>
+                base: "4xl",
+                md: "5xl",
+              }}
+            >
               Get in Touch
             </Heading>
 
             <Stack
               spacing={{ base: 4, md: 8, lg: 10 }}
-              direction={{ base: 'column', md: 'row' }}>
-
-              <SocialIcons/>
-
-              <Box
-                bg={'neutral.100'}
-                borderRadius="lg"
-                p={8}                
-                shadow="base">
-                <VStack spacing={5} textColor={'secondary.800'}>
-                  <FormControl isRequired>
+              direction={{ base: "column", md: "row" }}
+            >
+              <SocialIcons />
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <Box bg={"neutral.100"} borderRadius="lg" p={8} shadow="base">
+                  <VStack textColor={"secondary.800"}>
                     <FormLabel>Name</FormLabel>
-                    <InputGroup borderColor={'secondary.900'}>
+                    <InputGroup
+                      borderColor={errors ? "red.400" : "secondary.900"}
+                    >
                       <InputLeftElement>{<BsPerson />}</InputLeftElement>
-                      <Input type="text" name="name" placeholder="Your Name" />
+                      <Input
+                        type="text"
+                        name="name"
+                        {...register("name", { required: true })}
+                        placeholder="Your Name"
+                        aria-invalid={errors.name ? "true" : "false"}
+                      />
                     </InputGroup>
-                  </FormControl>
+                    {errors.name?.type === "required" && (
+                      <FormErrorMsg msg={"Name is required"} />
+                    )}
 
-                  <FormControl isRequired>
                     <FormLabel>Email</FormLabel>
-                    <InputGroup borderColor={'secondary.900'}>
+                    <InputGroup
+                      borderColor={errors.email ? "red.400" : "secondary.900"}
+                    >
                       <InputLeftElement>{<MdOutlineEmail />}</InputLeftElement>
                       <Input
                         type="email"
                         name="email"
+                        {...register("email", { required: true })}
                         placeholder="Your Email"
+                        aria-invalid={errors.email ? "true" : "false"}
                       />
                     </InputGroup>
-                  </FormControl>
+                    {errors.email?.type === "required" && (
+                      <FormErrorMsg msg={"Email is required"} />
+                    )}
 
-                  <FormControl isRequired>
                     <FormLabel>Message</FormLabel>
                     <Textarea
+                      id="message"
                       name="message"
+                      {...register("message", { required: true })}
                       placeholder="Your Message"
                       rows={6}
                       resize="none"
-                      borderColor={'secondary.900'}
+                      borderColor={errors.message ? "red.400" : "secondary.900"}
+                      aria-invalid={errors.message ? "true" : "false"}
                     />
-                  </FormControl>
-
-                  <Button                    
-                    bg="primary.100"                    
-                    _hover={{
-                      bg: 'primary.default',
-                    }}
-                    isFullWidth>
-                    Send Message
-                  </Button>
-                </VStack>
-              </Box>
+                    {errors.message?.type === "required" && (
+                      <FormErrorMsg msg={"Message is required"} />
+                    )}
+                    <Button
+                      bg="primary.100"
+                      _hover={{
+                        bg: "primary.default",
+                      }}
+                      type={"submit"}
+                    >
+                      Send Message
+                    </Button>
+                  </VStack>
+                </Box>
+              </form>
             </Stack>
           </VStack>
         </Box>
       </Box>
     </Flex>
-  )
-}
+  );
+};
 
-export default ContactMe
+export default ContactMe;
