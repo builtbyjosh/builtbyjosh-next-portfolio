cc1b3f5789fbee2fa05f9d614b01be261f70ef5a
builtbyjosh <joshuacollins912@gmail.com>
2023-03-16 12:13:15 -0500

    added project cards

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import ProjectCard from '../../components/ProjectCard'
+import { projectsList } from '../../data/projectList'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <>
+    {projectsList.map((project) => (
+        <ProjectCard key={project.id} project={project} />
+      ))}
+    </>
   )
 }
 
