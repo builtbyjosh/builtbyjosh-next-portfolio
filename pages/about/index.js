05acffb7fec6e87f6293babf7f0b5ac084a18a70
builtbyjosh <joshuacollins912@gmail.com>
2023-03-18 23:37:04 -0500

    cleanup

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Text, Flex, VStack, Heading, Stack } from "@chakra-ui/react";
-import SocialIcons from "../../components/SocialIcons";
+
 import Layout from "../../components/Layout";
 const AboutMe = () => {
   return <Layout heading={"A little about Me!"}></Layout>;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Layout from "../components/Layout";
 import { Box, Text } from "@chakra-ui/react";
 import Footer from "../components/Footer";
