95c00734f03da96b7d9741f96a5e61edf1d8603a
builtbyjosh <joshuacollins912@gmail.com>
2023-03-18 23:36:25 -0500

    fixed positioning

--- components/Footer.js
+++ components/Footer.js
@@ -1,45 +1,69 @@
 import React from "react";
-import { Flex, Text } from "@chakra-ui/react";
 import {
-  BsHouseFill,
-  BsFolderFill,
-  BsPersonFill,
-  BsFillEnvelopeFill,
-} from "react-icons/bs";
-import SocialIconButton from "./SocialIconButton";
+  Flex,
+  Text,
+  IconButton,
+  Tooltip,
+  useClipboard,
+} from "@chakra-ui/react";
+import { BsGithub, BsLinkedin, BsTwitter } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+import IconLink from "./IconLink";
 
 const Footer = () => {
+  const { hasCopied, onCopy } = useClipboard("joshuacollins912@gmail.com");
   return (
     <Flex
       w="full"
       alignItems="center"
       justifyContent="center"
-      position={"absolute"}
+      position={"fixed"}
       bottom={0}
+      bg={"secondary.500"}
     >
-      <Flex
-        w="full"
-        flexDir={{
-          base: "column",
-          sm: "row",
-        }}
-        align="center"
-        justify="space-between"
-        px="6"
-      >
-        <Text fontSize="xl" fontWeight="bold" color="primary.100">
+      <Flex w="full" align="center" justify="space-between" px="6">
+        <Text fontSize="lg" fontWeight="bold" color="primary.100">
           Built By Josh
         </Text>
 
-        <Text py={0} color="gray.800">
-          All rights reserved
-        </Text>
-
         <Flex mx="-2">
-          <SocialIconButton href={"/"} icon={<BsHouseFill size="28px" />} />
-          <SocialIconButton href={"/about"} icon={<BsPersonFill />} />
-          <SocialIconButton href={"/projects"} icon={<BsFolderFill />} />
-          <SocialIconButton href={"/contact"} icon={<BsFillEnvelopeFill />} />
+          <Tooltip
+            label={hasCopied ? "Email Copied!" : "Copy Email"}
+            closeOnClick={false}
+            hasArrow
+          >
+            <IconButton
+              aria-label="email"
+              variant="ghost"
+              size="lg"
+              fontSize="3xl"
+              icon={<MdEmail />}
+              color={"primary.100"}
+              _hover={{
+                bg: "primary.default",
+                color: "white",
+              }}
+              onClick={onCopy}
+              isRound
+            />
+          </Tooltip>
+
+          <IconLink
+            href={"https://github.com/builtbyjosh"}
+            icon={<BsGithub />}
+            isExternal={true}
+          />
+
+          <IconLink
+            href={"https://twitter.com/ThriftyCoder"}
+            icon={<BsTwitter size="28px" />}
+            isExternal={true}
+          />
+          <IconLink
+            href={"https://www.linkedin.com/in/joshuajohncollins/"}
+            icon={<BsLinkedin size="28px" />}
+            isExternal={true}
+          />
         </Flex>
       </Flex>
     </Flex>
