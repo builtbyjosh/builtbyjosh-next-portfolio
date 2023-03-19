996e24abc984d966451dc4501daf6774fed16c4e
builtbyjosh <joshuacollins912@gmail.com>
2023-03-18 22:47:14 -0500

    add layout component

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-
+import React from "react";
+import { Box, Text, Flex, VStack, Heading, Stack } from "@chakra-ui/react";
+import SocialIcons from "../../components/SocialIcons";
+import Layout from "../../components/Layout";
 const AboutMe = () => {
-  return (
-    <div>AboutMe</div>
-  )
-}
+  return <Layout heading={"A little about Me!"}></Layout>;
+};
 
-export default AboutMe
+export default AboutMe;
