b03695a7a09ca88acfcc9d85c9ffcc26078a2304
builtbyjosh <joshuacollins912@gmail.com>
2023-03-20 09:47:43 -0500

    code cleanup

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -1,14 +1,10 @@
 import sendgrid from "@sendgrid/mail";
-
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
-
 async function sendEmail(req, res) {
   try {
-    console.log("REQ.BODY", req.body);
-
     await sendgrid.send({
-      to: "joshuacollins912@gmail.com", // Your email where you'll receive emails
-      from: "contact@builtbyjosh.com", // your website email address here
+      to: "joshuacollins912@gmail.com",
+      from: "contact@builtbyjosh.com",
       subject: `${req.body.subject}`,
       html: `<div>
       <p>Name: ${req.body.name}</p>
@@ -17,11 +13,8 @@ async function sendEmail(req, res) {
       </div>`,
     });
   } catch (error) {
-    // console.log(error);
     return res.status(error.statusCode || 500).json({ error: error.message });
   }
-
   return res.status(200).json({ error: "" });
 }
-
 export default sendEmail;
