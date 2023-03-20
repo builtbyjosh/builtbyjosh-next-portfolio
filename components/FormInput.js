7d8870c2ba01f078a7365dedb165edef61bd50f9
builtbyjosh <joshuacollins912@gmail.com>
2023-03-20 10:24:45 -0500

    Put form input into own component

---
+++ components/FormInput.js
@@ -0,0 +1,31 @@
+import {
+  InputGroup,
+  Input,
+  InputLeftElement,
+  FormLabel,
+} from "@chakra-ui/react";
+import { Fragment } from "react";
+import FormErrorMsg from "./FormErrorMsg";
+
+const FormInput = ({ name, type = "text", errors, register, icon }) => {
+  return (
+    <Fragment>
+      <FormLabel>{name}</FormLabel>
+      <InputGroup borderColor={errors ? "red.400" : "secondary.900"}>
+        <InputLeftElement>{icon}</InputLeftElement>
+        <Input
+          type={type}
+          name={name.toLowerCase()}
+          {...register("name", { required: true })}
+          placeholder="Your Name"
+          aria-invalid={errors ? "true" : "false"}
+        />
+      </InputGroup>
+      {errors?.type === "required" && (
+        <FormErrorMsg msg={`${name} is required`} />
+      )}
+    </Fragment>
+  );
+};
+
+export default FormInput;
