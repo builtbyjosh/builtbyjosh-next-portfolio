5740a030499dcdb6250e31f13582c5e2a1c850bf
builtbyjosh <joshuacollins912@gmail.com>
2023-03-20 22:12:17 -0500

    updated project list

--- data/projectList.js
+++ data/projectList.js
@@ -1,37 +1,34 @@
 export const projectsList = [
-    {
-      id: 1,
-      title: 'Project1',
-      logo: 'https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&auto=format&fit=crop&w=334&q=80',
-      link: 'https://www.test.com',
-      desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-      technologies: ['rails', 'react']
-    },
-    {
-      id: 2,
-      title: 'Project2',
-      link: 'https://www.test.com',
-      logo: 'https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&auto=format&fit=crop&w=334&q=80',
-      desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.`,
-      technologies: ['ruby', 'javascript']
-    },
-    {
-      id: 3,
-      title: 'Project3',
-      link: 'https://www.test.com',
-      logo: 'https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&auto=format&fit=crop&w=334&q=80',
-      desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.`,
-      technologies: ['ruby', 'javascript']
-    },
-    {
-      id: 4,
-      title: 'Project4',
-      link: 'https://www.test.com',
-      logo: 'https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&auto=format&fit=crop&w=334&q=80',
-      desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.`,
-      technologies: ['ruby', 'javascript']
-    }
-  ];
+  {
+    id: 1,
+    title: "Tipsy List",
+    logo: "https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&auto=format&fit=crop&w=334&q=80",
+    link: "https://github.com/builtbyjosh/tipsy_list_v2",
+    desc: "The user can save breweries that are called from the OpenBreweryDB API. They can then create reviews for the brewery. The user will have a list of saved breweries to access the reviews.",
+    technologies: ["rails", "react", "redux"],
+  },
+  {
+    id: 2,
+    title: "Cigar Bar",
+    link: "https://github.com/builtbyjosh/cigar_bar",
+    logo: "https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&auto=format&fit=crop&w=334&q=80",
+    desc: "An online humidor where a user can keep track of the different cigars they currently have. The user can also sort the cigars by specific type.",
+    technologies: ["ruby", "javascript"],
+  },
+  {
+    id: 3,
+    title: "GameStoop",
+    link: "https://github.com/builtbyjosh/GameStoopV2",
+    logo: "https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&auto=format&fit=crop&w=334&q=80",
+    desc: "A simple online store with shopping cart and checkout. There is also user authentication with OAuth",
+    technologies: ["ruby", "ruby on rails", "active record"],
+  },
+  {
+    id: 4,
+    title: "Portfolio Site",
+    link: "https://github.com/builtbyjosh/builtbyjosh-next-portfolio",
+    logo: "https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&auto=format&fit=crop&w=334&q=80",
+    desc: "My personal portfolio site repo. Utilizing NextJS to create a static site. I also implimented Sendgrid for the contact form and deployed using vercel/",
+    technologies: ["nextjs", "chakra ui"],
+  },
+];
