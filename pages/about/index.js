b02c9aca0c3891d1aaf02314e7240e4463b60b1e
builtbyjosh <joshuacollins912@gmail.com>
2023-03-20 21:58:00 -0500

    added copy

--- pages/about/index.js
+++ pages/about/index.js
@@ -3,7 +3,25 @@ import { Box, Text, Flex, VStack, Heading, Stack } from "@chakra-ui/react";
 
 import Layout from "../../components/Layout";
 const AboutMe = () => {
-  return <Layout heading={"A little about Me!"}></Layout>;
+  return (
+    <Layout heading={"A little about Me!"}>
+      <Text>
+        As a software engineer with experience in full-stack web development, I
+        am passionate about creating innovative solutions that make a
+        difference. I have a strong background in front-end development,
+        specializing in building progressive web apps using modern technologies
+        like React, TypeScript, and Cypress. Additionally, my knowledge of
+        RESTful API, CRUD operations, and responsive web design enables me to
+        create functional and user-friendly web applications. I am proficient in
+        a wide range of building, testing, and version control tools like Git,
+        MongoDB, Ruby, and Rails. I take pride in my ability to manage and lead
+        a team of developers and am always excited to learn and stay up-to-date
+        with emerging technologies. With my extensive experience and passion for
+        software development, I am committed to delivering high-quality work
+        that meets and exceeds the expectations of clients and end-users alike.
+      </Text>
+    </Layout>
+  );
 };
 
 export default AboutMe;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
 /* eslint-disable react/no-unescaped-entities */
 import Layout from "../components/Layout";
-import { Box, Text } from "@chakra-ui/react";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import { Box, Text, Heading, Stack } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Layout heading={"Hi! Im Josh"}>
-      <Box>
-        <Text>I'm a Full Stack Developer</Text>
+    <Layout heading={"Welcome!"}>
+      <Box maxW={"2xl"}>
+        <Stack mb={4} direction={{ base: "row", md: "column" }}>
+          <Text color={"primary.300"}>Hey! My name is </Text>
+          <Heading size={"md"}>Joshua Collins</Heading>
+        </Stack>
+        <Text fontSize={"lg"} color={"primary.300"}>
+          I am a software developer specializing in Front End Development based
+          in Chicago, Il.{" "}
+        </Text>
+        <br />
+        <Text color={"primary.300"}>
+          I am passionate about software development and take pride in creating
+          responsive and engaging websites that are not only visually appealing
+          but also functional and user-friendly."
+        </Text>
       </Box>
     </Layout>
   );
