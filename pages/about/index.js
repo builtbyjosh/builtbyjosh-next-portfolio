61111e965faa67b9a67ad1221e87fc4081f00a1a
builtbyjosh <joshuacollins912@gmail.com>
2023-03-23 16:12:33 -0500

    Build out about me page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,25 +1,65 @@
 import React from "react";
-import { Box, Text, Flex, VStack, Heading, Stack } from "@chakra-ui/react";
+import { Box, Text, Stack, SimpleGrid, Tag } from "@chakra-ui/react";
 
 import Layout from "../../components/Layout";
 const AboutMe = () => {
+  const skillList = [
+    "React",
+    "TypeScript",
+    "JavaScript",
+    "NextJS",
+    "Chakra-UI",
+    "Cypress",
+  ];
   return (
     <Layout heading={"A little about Me!"}>
-      <Text>
-        As a software engineer with experience in full-stack web development, I
-        am passionate about creating innovative solutions that make a
-        difference. I have a strong background in front-end development,
-        specializing in building progressive web apps using modern technologies
-        like React, TypeScript, and Cypress. Additionally, my knowledge of
-        RESTful API, CRUD operations, and responsive web design enables me to
-        create functional and user-friendly web applications. I am proficient in
-        a wide range of building, testing, and version control tools like Git,
-        MongoDB, Ruby, and Rails. I take pride in my ability to manage and lead
-        a team of developers and am always excited to learn and stay up-to-date
-        with emerging technologies. With my extensive experience and passion for
-        software development, I am committed to delivering high-quality work
-        that meets and exceeds the expectations of clients and end-users alike.
-      </Text>
+      <Stack textAlign={"left"} color={"primary.500"} spacing={5}>
+        <Text>
+          I first became intersted in Software development in 2018. I started
+          working through freeCodeCamps learning modules and then moved onto
+          Udemy courses.
+        </Text>
+        <Text>
+          Fast forward a couple of years, I decided to attend the Flatiron
+          Schools Software Engineering Coding Bootcamp. Shortly after completing
+          my bootcamp, I landed my first role as a Frontend Developer. And had
+          to quickly get up to speed working with TypeScript and Chakra-UI,
+          neither of which I had ever worked with before.
+        </Text>
+        <Text>
+          All of this has led me to become a seasoned React developer with
+          experience in developing high-quality web applications. I am skilled
+          in front-end development, including creating reusable UI components
+          and integrating with back-end APIs. I am passionate about creating
+          user-friendly and responsive interfaces that enhance the user
+          experience.
+        </Text>
+
+        <Text>
+          Here are some of the technologies I have been working with recently:
+        </Text>
+        <Box w={"100%"}>
+          <SimpleGrid
+            columns={{ base: 2, sm: 3 }}
+            rows={{ base: 2, sm: 3 }}
+            spacing={5}
+          >
+            {skillList.map((skill, index) => (
+              <Text
+                textAlign={"center"}
+                key={index}
+                size={"md"}
+                bg={"primary.200"}
+                border={"1px"}
+                borderRadius={"lg"}
+                // minW={{base: "150px",}}
+              >
+                {skill}
+              </Text>
+            ))}
+          </SimpleGrid>
+        </Box>
+      </Stack>
     </Layout>
   );
 };
