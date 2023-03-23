68b4131205d623f7e14d237f138c6d756ec0b5ac
builtbyjosh <joshuacollins912@gmail.com>
2023-03-23 17:27:53 -0500

    add page transition animation

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 // import '../styles/globals.css';
-import { ChakraProvider} from '@chakra-ui/react';
+import { ChakraProvider } from "@chakra-ui/react";
 // import theme from '../styles/themes'
-import theme from '../styles/theme';
+import theme from "../styles/theme";
+import { AnimatePresence } from "framer-motion";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <AnimatePresence mode="wait" initial={false}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AnimatePresence>
   );
 }
 
