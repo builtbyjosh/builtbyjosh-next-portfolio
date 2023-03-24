d94513826f5737bf3c2f49d55bc70a59b27f0172
builtbyjosh <joshuacollins912@gmail.com>
2023-03-24 09:53:07 -0500

    fix page sizing

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Text, Stack, SimpleGrid, Tag } from "@chakra-ui/react";
+import { Box, Text, Stack, SimpleGrid, Container } from "@chakra-ui/react";
 
 import Layout from "../../components/Layout";
 const AboutMe = () => {
@@ -13,58 +13,56 @@ const AboutMe = () => {
   ];
   return (
     <Layout heading={"A little about Me!"}>
-      <Stack
-        transition={"ease-in"}
-        textAlign={"left"}
-        color={"primary.500"}
-        spacing={5}
-      >
-        <Text>
-          I first became intersted in Software development in 2018. I started
-          working through freeCodeCamps learning modules and then moved onto
-          Udemy courses.
-        </Text>
-        <Text>
-          Fast forward a couple of years, I decided to attend the Flatiron
-          Schools Software Engineering Coding Bootcamp. Shortly after completing
-          my bootcamp, I landed my first role as a Frontend Developer. And had
-          to quickly get up to speed working with TypeScript and Chakra-UI,
-          neither of which I had ever worked with before.
-        </Text>
-        <Text>
-          All of this has led me to become a seasoned React developer with
-          experience in developing high-quality web applications. I am skilled
-          in front-end development, including creating reusable UI components
-          and integrating with back-end APIs. I am passionate about creating
-          user-friendly and responsive interfaces that enhance the user
-          experience.
-        </Text>
+      <Container maxW="2xl" p={2}>
+        <Stack textAlign={"left"} color={"primary.500"} spacing={5}>
+          <Text>
+            I first became intersted in Software development in 2018. I started
+            working through freeCodeCamps learning modules and then moved onto
+            Udemy courses.
+          </Text>
+          <Text>
+            Fast forward a couple of years, I decided to attend the Flatiron
+            Schools Software Engineering Coding Bootcamp. Shortly after
+            completing my bootcamp, I landed my first role as a Frontend
+            Developer. And had to quickly get up to speed working with
+            TypeScript and Chakra-UI, neither of which I had ever worked with
+            before.
+          </Text>
+          <Text>
+            All of this has led me to become a seasoned React developer with
+            experience in developing high-quality web applications. I am skilled
+            in front-end development, including creating reusable UI components
+            and integrating with back-end APIs. I am passionate about creating
+            user-friendly and responsive interfaces that enhance the user
+            experience.
+          </Text>
 
-        <Text>
-          Here are some of the technologies I have been working with recently:
-        </Text>
-        <Box w={"100%"}>
-          <SimpleGrid
-            columns={{ base: 2, sm: 3 }}
-            rows={{ base: 2, sm: 3 }}
-            spacing={5}
-          >
-            {skillList.map((skill, index) => (
-              <Text
-                textAlign={"center"}
-                key={index}
-                size={"md"}
-                bg={"primary.200"}
-                border={"1px"}
-                borderRadius={"lg"}
-                // minW={{base: "150px",}}
-              >
-                {skill}
-              </Text>
-            ))}
-          </SimpleGrid>
-        </Box>
-      </Stack>
+          <Text>
+            Here are some of the technologies I have been working with recently:
+          </Text>
+          <Box w={"100%"}>
+            <SimpleGrid
+              columns={{ base: 2, sm: 3 }}
+              rows={{ base: 2, sm: 3 }}
+              spacing={5}
+            >
+              {skillList.map((skill, index) => (
+                <Text
+                  textAlign={"center"}
+                  key={index}
+                  size={"md"}
+                  bg={"primary.200"}
+                  border={"1px"}
+                  borderRadius={"lg"}
+                  // minW={{base: "150px",}}
+                >
+                  {skill}
+                </Text>
+              ))}
+            </SimpleGrid>
+          </Box>
+        </Stack>
+      </Container>
     </Layout>
   );
 };
