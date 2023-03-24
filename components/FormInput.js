b3e13e3c531f04575cb26d69ab2f1726696fb08a
builtbyjosh <joshuacollins912@gmail.com>
2023-03-24 09:52:17 -0500

    Cleanup and color fix

--- components/FormInput.js
+++ components/FormInput.js
@@ -4,27 +4,27 @@ import {
   InputLeftElement,
   FormLabel,
 } from "@chakra-ui/react";
-import { Fragment } from "react";
 import FormErrorMsg from "./FormErrorMsg";
 
 const FormInput = ({ name, type = "text", errors, register, icon }) => {
   return (
-    <Fragment>
+    <>
       <FormLabel>{name}</FormLabel>
-      <InputGroup borderColor={errors ? "red.400" : "secondary.900"}>
+      <InputGroup borderColor={errors ? "red.400" : "primary.500"}>
         <InputLeftElement>{icon}</InputLeftElement>
         <Input
           type={type}
           name={name.toLowerCase()}
           {...register(`${name.toLowerCase()}`, { required: true })}
           placeholder="Your Name"
+          _placeholder={{ color: "primary.500" }}
           aria-invalid={errors ? "true" : "false"}
         />
       </InputGroup>
       {errors?.type === "required" && (
         <FormErrorMsg msg={`${name} is required`} />
       )}
-    </Fragment>
+    </>
   );
 };
 
