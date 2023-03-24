7e80851993b824245de7a98a7355673925058aa3
builtbyjosh <joshuacollins912@gmail.com>
2023-03-24 09:53:17 -0500

    fix page sizing

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,10 +1,22 @@
 import ContactForm from "../../components/ContactForm";
 import Layout from "../../components/Layout";
+import { Container, Divider, Text } from "@chakra-ui/layout";
 
 const ContactMe = () => {
   return (
     <Layout heading={"Get in Touch"}>
-      <ContactForm />
+      <Container maxW="2xl" p={2}>
+        <Text textColor={"primary.500"}>
+          Please feel free to reach out if you have any questions for me or if
+          you would like to discuss helping you create a website
+        </Text>
+        <Divider
+          my={5}
+          orientation="horizontal"
+          borderColor={"secondary.900"}
+        />
+        <ContactForm />
+      </Container>
     </Layout>
   );
 };
