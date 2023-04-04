cfa95d45a03bb834c78d5aec6830092ef540c570
builtbyjosh <joshuacollins912@gmail.com>
2023-04-04 13:21:08 -0500

    possible bug fix

--- pages/projects/[uid].js
+++ pages/projects/[uid].js
@@ -6,45 +6,39 @@ const ProjectPage = ({ projectData }) => {
   const { title, desc, link, technologies } = projectData;
 
   return (
-    <>
-      {projectData && (
-        <Layout heading={"Project Details"}>
-          <Container maxW={"2xl"} p={2}>
-            <Heading>{title}</Heading>
-            <Stack direction={"row"} spacing="1">
-              {technologies.map((tech, index) => (
-                <Tag p={1} key={index} size="sm" bg={"primary.200"}>
-                  {tech}
-                </Tag>
-              ))}
-            </Stack>
-            <Text>{desc}</Text>
-            <Link isExternal href={link}>
-              Github Repo
-            </Link>
-          </Container>
-        </Layout>
-      )}
-    </>
+    <Layout heading={"Project Details"}>
+      <Container maxW={"2xl"} p={2}>
+        <Heading>{title}</Heading>
+        <Stack direction={"row"} spacing="1">
+          {technologies.map((tech, index) => (
+            <Tag p={1} key={index} size="sm" bg={"primary.200"}>
+              {tech}
+            </Tag>
+          ))}
+        </Stack>
+        <Text>{desc}</Text>
+        <Link isExternal href={link}>
+          Github Repo
+        </Link>
+      </Container>
+    </Layout>
   );
 };
 
 export default ProjectPage;
 
-export async function getStaticPaths() {
-  const projects = await getAllProjects();
-  const paths = projects.map((project) => ({
-    params: { uid: project.uid },
-  }));
-  return {
-    paths,
-    fallback: true,
-  };
-}
+// export async function getStaticPaths() {
+//   const projects = await getAllProjects();
+//   const paths = projects.map((project) => ({
+//     params: { uid: project.uid },
+//   }));
+//   return {
+//     paths,
+//     fallback: true,
+//   };
+// }
 
-export async function getStaticProps({ params: { uid } }) {
-  console.log("PROJECT ID:", uid);
+export async function getServerSideProps({ params: { uid } }) {
   const projectData = await getProjectById(uid);
-  console.log("fetched project", projectData);
   return { props: { projectData: projectData }, revalidate: 1 };
 }
