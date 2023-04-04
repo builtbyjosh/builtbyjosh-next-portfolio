44556a76e24cbb2607eb0c703f9beb3eac7410c7
builtbyjosh <joshuacollins912@gmail.com>
2023-04-04 13:13:03 -0500

    removed unused code

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,9 +1,8 @@
-import { useState, useEffect } from "react";
 import ProjectCard from "../../components/ProjectCard";
 import Layout from "../../components/Layout";
 import { Container } from "@chakra-ui/layout";
 import { getAllProjects } from "../api/projects";
-import Link from "next/link";
+
 const Projects = ({ projects }) => {
   return (
     <Layout heading={"Some of My Work"}>
@@ -22,13 +21,3 @@ export async function getServerSideProps() {
   const projects = await getAllProjects();
   return { props: { projects } };
 }
-
-// const [projects, setProjects] = useState([]);
-// useEffect(() => {
-//   const fetchData = async () => {
-//     const fetchedProjects = await getAllProjects();
-//     setProjects(fetchedProjects);
-//   };
-
-//   fetchData();
-// }, []);
